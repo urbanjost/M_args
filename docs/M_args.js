1780869db39c2c8024e4f95882b388c208fbb144
John S. Urban <urbanjost@comcast.net>
2021-03-28 18:22:26 -0400

    init

---
+++ docs/M_args.js
@@ -0,0 +1,8 @@
+function loadthem(){
+append("M_args.3m_args.html");
+append("get_command_arguments_stack.3m_args.html");
+append("get_command_arguments_string.3m_args.html");
+append("get_namelist.3m_args.html");
+append("longest_command_argument.3m_args.html");
+append("print_dictionary.3m_args.html");
+}
